ea988f8d1aa4f276b13dc2b383fcbb1c1ea6cba5
sindrew <sindrew.kodehode@gmail.com>
2024-01-26 09:11:55 +0100

    try IP

--- src/src/Context/Context.jsx
+++ src/src/Context/Context.jsx
@@ -45,7 +45,7 @@ export const refreshToken = async () => {
         withCredentials: true,
     };
     try {
-        const refresh = await axios.post('https://c7db-20-223-156-203.ngrok-free.app/refresh', {}, config);
+        const refresh = await axios.post('https://20.223.156.203:443/refresh', {}, config);
 
 
     } catch (error) {
