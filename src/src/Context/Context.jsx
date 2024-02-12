a9355d056aecf97478fe518a920b133d32978ff8
sindrew <sindrew.kodehode@gmail.com>
2024-02-12 09:32:37 +0100

    test auth on checkloginstate

--- src/src/Context/Context.jsx
+++ src/src/Context/Context.jsx
@@ -71,7 +71,7 @@ export function setLoginState(user) {
     localStorage.setItem("loginState", JSON.stringify(item));
 };
 
-export function checkLoginState() {
+export async function checkLoginState() {
     const itemStr = localStorage.getItem("loginState");
 
     if (!itemStr) {
@@ -80,6 +80,18 @@ export function checkLoginState() {
 
     const item = JSON.parse(itemStr);
     const now = new Date();
+
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true,
+        };
+       return await axios.post('https://aivispitchstackserver.azurewebsites.net/auth', JSON.stringify(item), config);
+    } catch (error) {
+        console.error("Error fetching cookie:", error);
+    }
 
     if (now.getTime() > item.expiry) {
         localStorage.removeItem("loginState");
