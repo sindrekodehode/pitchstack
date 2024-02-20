f8f4d04e6b13994681725daac16d828c453f285a
sindrew <sindrew.kodehode@gmail.com>
2024-02-20 08:14:51 +0100

    new test

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,13 @@ import { Dropbox } from './src/Dropbox/Dropbox'
 import { Stats } from './src/Stats/Stats'
 import { Res } from './src/Res/Res'
 import axios from 'axios';
-import { ContextProvider, refreshToken } from './src/Context/Context'
 
 
 
 function App() {
 
-  useEffect( async ()  => {
-    const itemStr = JSON.parse(localStorage.getItem("loginState"));
-
-    if (!itemStr) {
-        return { hasSubmitted: false };
-    }
-
-    if (itemStr.hasSubmitted === true) {
-
-      try {
-          const config = {
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              withCredentials: true,
-          };
-         return await axios.post('https://aivispitchstackserver.azurewebsites.net/auth', JSON.stringify(item), config);
-      } catch (error) {
-          console.error("Error fetching cookie:", error);
-      }
-    }
-})
-
-
   return (
     <Router>
-      <ContextProvider>
         <div>
           <Header />
           <Routes>
@@ -48,7 +22,6 @@ function App() {
             <Route path="*" element= {<h1>404 Not Found</h1>}/>
           </Routes>
         </div>
-      </ContextProvider>
     </Router>
   )
 }
