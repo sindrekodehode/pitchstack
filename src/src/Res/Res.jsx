ebb98a8b69c1236e5f29fbfc07fac22e0639e465
sindrew <sindrew.kodehode@gmail.com>
2024-02-26 15:29:03 +0100

    small fix

--- src/src/Res/Res.jsx
+++ src/src/Res/Res.jsx
@@ -71,24 +71,31 @@ export function Res() {
     return (
         <div className={styles.container}>
             <div className={styles.stats}>
+            <div key={index} className={styles.pdfcontainer}>
+                <div className={styles.pdfstats}>
                 <div className={styles.score}><h2>Your pitchscore </h2><div className={styles.scoreNum}>{score}</div></div>
-               {Object.entries(response).map(([key, value]) => (
-                <div key={key} className={styles.itemCard}>
-                <div className={styles.cardText}>
-                <div className={styles.cardTextHeaderContainer}>
-                    <div className={styles.colorBox} >
-                        <img src={ getColor(value.rating) } alt='crab color rating image'></img>
-                    </div>
-                    <div className={styles.textcontainer}>
-                        <h3>{key}:</h3>
-                    </div>
+                <div className={styles.infographic}><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore inventore esse, labore hic optio, nam explicabo, voluptates modi officiis suscipit assumenda veritatis repudiandae ipsam amet porro? Fugit aspernatur quas quam expedita facilis suscipit, nulla, dolorem reiciendis numquam itaque beatae. Necessitatibus, nam in! Quisquam necessitatibus tenetur fuga obcaecati dignissimos unde harum.</p></div>
+               
+                        {Object.entries(response).map(([key, value]) => (
+                            <div key={key} className={styles.itemCard}>
+                            <div className={styles.cardText}>
+                            <div className={styles.cardTextHeaderContainer}>
+                                <div className={styles.colorBox} >
+                                    <img src={ getColor(value.rating) } alt='crab color rating image'></img>
+                                </div>
+                                <div className={styles.textcontainer}>
+                                    <h3>{key}:</h3>
+                                </div>
+                            </div>
+                            <p>{value.item}</p>
+                            <p>{value.evaluation}</p>
+                            </div>
+                            
+                        </div>
+                        ))}
+                        
+                   </div>
                 </div>
-                <p>{value.item}</p>
-                <p>{value.evaluation}</p>
-                </div>
-                
-            </div>
-               ))}
             </div>
         </div>
     )
