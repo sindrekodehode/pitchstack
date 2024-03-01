e32ac7777944863fe6afea7aced9a681056e7c1e
sindrew <sindrew.kodehode@gmail.com>
2024-03-01 09:01:16 +0100

    ?

--- src/src/Footer/Footer.jsx
+++ src/src/Footer/Footer.jsx
@@ -20,9 +20,9 @@ export function Footer() {
                 <div className={styles.info}>
                     <ul>
                         <h2>Informasjon</h2>
-                        <li><a href="https://www.visinnovasjon.no/personvernerklaering/">Personvernerklæring</a></li>
-                        <li><a href="https://www.visinnovasjon.no/cookie-policy/">Cookies</a></li>
-                        <li><a href="https://www.visinnovasjon.no/cookie-policy/">Åpenhetsloven</a></li>
+                        <li><a href="https://www.visinnovasjon.no/personvernerklaering/"target="_blank" rel="noopener noreferrer">Personvernerklæring</a></li>
+                        <li><a href="https://www.visinnovasjon.no/cookie-policy/"target="_blank" rel="noopener noreferrer">Cookies</a></li>
+                        <li><a href="https://www.visinnovasjon.no/cookie-policy/"target="_blank" rel="noopener noreferrer">Åpenhetsloven</a></li>
                         <li>org. nr: 987 753 153</li>
                     </ul>
                 </div>
