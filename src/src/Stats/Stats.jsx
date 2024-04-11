8cd22248fd6b8fe45dde625e977b3c58a6a526e4
sindrew <sindrew.kodehode@gmail.com>
2024-04-11 12:10:13 +0200

    wut

--- src/src/Stats/Stats.jsx
+++ src/src/Stats/Stats.jsx
@@ -156,7 +156,7 @@ export function Stats() {
             <Aside />
             {hasSubmitted && (
                 <div className={styles.stats}>
-                    {selectedPDFData.length > 0 && selectedPDFData[0]?.data.map((pdfData, index) => {
+                    {selectedPDFData && selectedPDFData[0]?.data.map((pdfData, index) => {
                         const weightedScore = calculateWeightedScore(pdfData.data, ratings);
                         return (
                         <div key={index} className={styles.pdfcontainer}>
