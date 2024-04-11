0a2c54c904f80a1c4db20cc553b87923428d920d
sindrew <sindrew.kodehode@gmail.com>
2024-04-11 14:57:10 +0200

    try

--- src/src/Res/Res.jsx
+++ src/src/Res/Res.jsx
@@ -97,7 +97,7 @@ export function Res() {
         
     };
 
-    const weightedScore = calculateWeightedScore(response.data, ratings)
+    const weightedScore = calculateWeightedScore(response?.data, ratings)
     
     return (
         <div className={styles.container}>
@@ -107,7 +107,7 @@ export function Res() {
                 <div className={styles.score}><h2>Your pitchscore </h2><div className={styles.scoreNum}>{weightedScore}</div></div>
                 <div className={styles.infographic}><p>The pitchstack replicates a VC fundmember that is an industry expert. In a similar way to how one expert might give a different score than another when reviewing a pitchstack the pitchstack AI will also vary in its scoring.  When weighted against previous pitchstacks that have done well and ones that have done poorly, there is a good correlation between scoring highly and the subsequent success of the startup.  A score between 0-35 is considered poor, 35-50 good and over 50 is excellent.</p></div>
                
-                        {Object.entries(response.data).map(([key, value]) => (
+                        {response?.data && Object.entries(response.data).map(([key, value]) => (
                             <div key={key} className={styles.itemCard} onClick={() => clickDiv(key)} style={activeDiv === key ? {zindex: 10, transform: 'scale(1.5)', background: '#020202', height: '-webkit-fit-content', height: '-moz-fit-content', height: 'fit-content'} : {}}>
                             <div className={styles.cardText}>
                             <div className={styles.cardTextHeaderContainer}>
