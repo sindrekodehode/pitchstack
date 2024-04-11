db9a42df1ddf24f76036c2761ae6168b09f6493b
sindrew <sindrew.kodehode@gmail.com>
2024-04-11 15:10:09 +0200

    try?

--- src/src/Res/Res.jsx
+++ src/src/Res/Res.jsx
@@ -103,17 +103,17 @@ export function Res() {
             console.log(typeof response);
         }
     })
-    // const weightedScore = calculateWeightedScore(response?.data, ratings)
+    const weightedScore = calculateWeightedScore(response, ratings)
     
     return (
         <div className={styles.container}>
             <div className={styles.stats}>
             <div className={styles.pdfcontainer}>
                 <div className={styles.pdfstats}>
-                <div className={styles.score}><h2>Your pitchscore </h2><div className={styles.scoreNum}></div></div>
+                <div className={styles.score}><h2>Your pitchscore </h2><div className={styles.scoreNum}>{weightedScore}</div></div>
                 <div className={styles.infographic}><p>The pitchstack replicates a VC fundmember that is an industry expert. In a similar way to how one expert might give a different score than another when reviewing a pitchstack the pitchstack AI will also vary in its scoring.  When weighted against previous pitchstacks that have done well and ones that have done poorly, there is a good correlation between scoring highly and the subsequent success of the startup.  A score between 0-35 is considered poor, 35-50 good and over 50 is excellent.</p></div>
-               
-                        {/* {response?.data && Object.entries(response.data).map(([key, value]) => (
+
+                        {response && Object.entries(response).map(([key, value]) => (
                             <div key={key} className={styles.itemCard} onClick={() => clickDiv(key)} style={activeDiv === key ? {zindex: 10, transform: 'scale(1.5)', background: '#020202', height: '-webkit-fit-content', height: '-moz-fit-content', height: 'fit-content'} : {}}>
                             <div className={styles.cardText}>
                             <div className={styles.cardTextHeaderContainer}>
@@ -129,7 +129,7 @@ export function Res() {
                             </div>
                             
                         </div>
-                        ))} */}
+                        ))}
 
                    </div>
                 </div>
